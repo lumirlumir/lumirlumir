ddb8a0b1718665570c576213e9aaec5e74d9698c
루밀LuMir <rpfos@naver.com>
2025-03-29 23:58:55 +0900

    wip: create `constants.mjs`

--- src/core/constants.mjs
+++ src/core/constants.mjs
@@ -1,1 +1,20 @@
-export const URL_GITHUB = 'https://github.com/';
+/**
+ * @fileoverview Constants.
+ */
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
+
+/** @type {string} */
+export const URL_GITHUB = 'https://github.com';
+/** @type {string} */
+export const URL_GITHUB_LUMIR = `${URL_GITHUB}/lumirlumir`;
+
+/** @type {string} */
+export const COMMENT_DO_NOT_EDIT =
+  '<!-- This file is auto-generated. Please do not edit it directly. -->';
+
+/** @type {string} */
+export const ICON_MERGED =
+  '<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill="#ab7df8" d="M5.45 5.154A4.25 4.25 0 0 0 9.25 7.5h1.378a2.251 2.251 0 1 1 0 1.5H9.25A5.734 5.734 0 0 1 5 7.123v3.505a2.25 2.25 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.95-.218ZM4.25 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm8.5-4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM5 3.25a.75.75 0 1 0 0 .005V3.25Z"></path></svg>';
