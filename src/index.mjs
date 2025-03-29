6810e04d27a7c4f7cd31c49828c228f07fae4ccb
루밀LuMir <rpfos@naver.com>
2025-03-30 02:05:53 +0900

    feat(*): add other legacy contribution list

--- src/index.mjs
+++ src/index.mjs
@@ -24,6 +24,7 @@ import organizations from './data/contributions.mjs';
 // --------------------------------------------------------------------------------
 
 /**
+ * @typedef {import('./core/types.d.ts').Organization} Organization
  * @typedef {import('./core/types.d.ts').PullRequest} PullRequest
  */
 
@@ -32,6 +33,49 @@ import organizations from './data/contributions.mjs';
 // --------------------------------------------------------------------------------
 
 const outputFilePath = resolve(process.cwd(), 'docs', 'contributions.md');
+
+/**
+ * Count total merged pull requests across all organizations.
+ * @param {Organization[]} orgs
+ * @returns {number}
+ */
+function countTotalMergedPRs(orgs) {
+  let count = 0;
+
+  for (const org of orgs) {
+    for (const repo of org.repositories) {
+      for (const pr of repo.pullRequests) {
+        if (pr.merged) count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+/**
+ * Count total contributed repositories across all organizations.
+ * @param {Organization[]} orgs
+ * @returns
+ */
+function countTotalContributedRepos(orgs) {
+  let count = 0;
+
+  for (const org of orgs) {
+    count += org.repositories.length;
+  }
+
+  return count;
+}
+
+/**
+ * Count total contributed organizations.
+ * @param {Organization[]} orgs
+ * @returns {number}
+ */
+function countTotalContributedOrgs(orgs) {
+  return orgs.length;
+}
 
 // --------------------------------------------------------------------------------
 // Scripts
@@ -44,6 +88,21 @@ let markdown = `${COMMENT_DO_NOT_EDIT}
 Every contribution I have made to open source projects is listed here.
 
 Copyright © 2024-${new Date().getFullYear()} [루밀LuMir(lumirlumir)](${URL_GITHUB_LUMIR}). All rights reserved.
+
+## Overview
+
+| Total Merged PRs                      | Total Contributed Repositories               | Total Contributed Organizations             |
+| :-----------------------------------: | :------------------------------------------: | :-----------------------------------------: |
+| ${countTotalMergedPRs(organizations)} | ${countTotalContributedRepos(organizations)} | ${countTotalContributedOrgs(organizations)} |
+
+## How to Read This Document
+
+| Emoji          | Description                |
+| -------------- | -------------------------- |
+| :purple_heart: | Successfully merged        |
+| :green_heart:  | Still open but meaningful  |
+
+# Details
 
 `;
 
@@ -63,7 +122,7 @@ organizations.forEach(organization => {
           markdown += `#### ${typeToTitle(type)}\n\n`;
         }
 
-        markdown += `1. ${merged ? ':purple_heart:' : ''} ${title} [#${number}](${URL_GITHUB_PULL_REQUEST(organization.name, repository.name, number)})\n\n`;
+        markdown += `1. ${title} [#${number}](${URL_GITHUB_PULL_REQUEST(organization.name, repository.name, number)}) ${merged ? ':purple_heart:' : ''}\n`;
       },
     );
   });
