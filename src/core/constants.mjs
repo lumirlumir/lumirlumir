9a68b1dbed98da2a2d95c2c21a81c914cb7e0cb6
루밀LuMir <rpfos@naver.com>
2025-03-30 01:06:15 +0900

    wip

--- src/core/constants.mjs
+++ src/core/constants.mjs
@@ -10,11 +10,15 @@
 export const URL_GITHUB = 'https://github.com';
 /** @type {string} */
 export const URL_GITHUB_LUMIR = `${URL_GITHUB}/lumirlumir`;
+/** @param {string} organization */
+export const URL_GITHUB_ORGANIZATION = organization => `${URL_GITHUB}/${organization}`;
+/** @param {string} organization @param {string} repository */
+export const URL_GITHUB_REPOSITORY = (organization, repository) =>
+  `${URL_GITHUB_ORGANIZATION(organization)}/${repository}`;
+/** @param {string} organization @param {string} repository @param {number} number */
+export const URL_GITHUB_PULL_REQUEST = (organization, repository, number) =>
+  `${URL_GITHUB_REPOSITORY(organization, repository)}/pull/${number}`;
 
 /** @type {string} */
 export const COMMENT_DO_NOT_EDIT =
-  '<!-- This file is auto-generated. Please do not edit it directly. -->';
-
-/** @type {string} */
-export const ICON_MERGED =
-  '<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill="#ab7df8" d="M5.45 5.154A4.25 4.25 0 0 0 9.25 7.5h1.378a2.251 2.251 0 1 1 0 1.5H9.25A5.734 5.734 0 0 1 5 7.123v3.505a2.25 2.25 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.95-.218ZM4.25 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm8.5-4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM5 3.25a.75.75 0 1 0 0 .005V3.25Z"></path></svg>';
+  '<!-- This file is auto-generated. Please do not edit it directly. -->\n\n<!-- eslint-disable -->\n<!-- markdownlint-disable -->';
