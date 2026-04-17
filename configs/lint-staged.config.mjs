f986800cd7482cdb7187885a6fcc45619b11a34f
루밀LuMir <rpfos@naver.com>
2026-04-17 23:10:58 +0900

    Simplify markdownlint command in lint-staged config

--- configs/lint-staged.config.mjs
+++ configs/lint-staged.config.mjs
@@ -4,5 +4,5 @@ export default {
     'editorconfig-checker -config .editorconfig-checker.json',
   ],
   '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,jsonc,json5,md}': 'eslint --fix',
-  '*.md': ['markdownlint --fix'],
+  '*.md': 'markdownlint --fix',
 };
